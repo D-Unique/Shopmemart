aba1a8d1769fba9802a687a0977f0f1118c5b6d2
Unigwe <emmunigwe@gmail.com>
2024-12-20 10:30:04 +0100

    modularise the styles

---
+++ frontend/src/components/Banner.tsx
@@ -0,0 +1,24 @@
+import '../styles/components/Banner.css';
+
+
+function Banner() {
+  return (
+      <div id="banner" >
+          <div className="text">
+          <h2 className='slideinleft'>shopping and department store.</h2>
+
+          <p className='slideinleft'>
+            Shopping is a bit of a relaxing hobby for me, which is sometimes
+            troubling for the bank balance.
+          </p>
+
+          <button>Learn More</button>
+          <div className='overlay slideinright' >
+        
+              </div>
+              </div>
+    </div>
+  );
+}
+
+export default Banner;
