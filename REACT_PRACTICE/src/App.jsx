86b9ee2ee979685819d9a3d86d99f41cc0843640
Gadoskey <gadoski321@gmail.com>
2024-12-24 16:14:47 +0100

    created react app

---
+++ REACT_PRACTICE/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Footer } from './components/Footer';
+import './index.css';
+import './components/Media.css';
+
+
+export default function App() {
+  return (
+    <>
+      <Footer />
+    </>
+  );
+}
