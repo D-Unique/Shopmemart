f172c0fd18da79e6f06a3aaa761f8286b3b60f04
Gadoskey <gadoski321@gmail.com>
2025-01-03 03:58:07 +0100

    goodnight

--- REACT_PRACTICE/src/components/Banner.tsx
+++ REACT_PRACTICE/src/components/Banner.tsx
@@ -1,38 +1,62 @@
 import '../styles/components/Banner.css';
 import { Button } from 'react-bootstrap';
+import SentenceDisplay from './SentenceDisplay';
 import Carousel from './Carousel';
+
+//Will find a way to reduce these imports and use beter images that fits well
 import image1 from '../assets/img/cart/carousel/image1.jpeg';
 import image2 from '../assets/img/cart/carousel/image2.jpeg';
 import image3 from '../assets/img/cart/carousel/image3.jpeg';
+import image4 from '../assets/img/cart/carousel/image4.jpeg';
+import image5 from '../assets/img/cart/carousel/image5.jpeg';
+import image6 from '../assets/img/cart/carousel/image6.jpeg';
+import image7 from '../assets/img/cart/carousel/image7.jpeg';
+import image8 from '../assets/img/cart/carousel/image8.jpeg';
+import image9 from '../assets/img/cart/carousel/image9.jpeg';
 
-const images: string[] = [image1, image2, image3];
+const images: string[] = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9
+];
 
 function Banner() {
   return (
-    <div id="banner">
-      <div className="text">
-        <h2 className="slideinleft">Shopping and Department Store</h2>
+    <div>
+      <div id="banner">
+        <div className="text">
+          <h2 className="slideinleft">Shopping and Department Store</h2>
 
-        <p className="intro2 slideinleft">
-          Explore our curated selection of products designed to fit your lifestyle and budget
-        </p>
+          <p className="intro2 slideinleft">
+            Explore our curated selection of products designed to fit your lifestyle and budget
+          </p>
 
-        <Button
-          className="intro2 slideinleft"
-          variant="primary"
-          style={{
-            transform:
-              'translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)',
-            transformStyle: 'preserve-3d',
-          }}
-        >
-          Start Shopping
-        </Button>
+          <Button
+            className="intro2 slideinleft"
+            style={{
+              transform:
+                'translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)',
+              transformStyle: 'preserve-3d',
+            }}
+          >
+            Start Shopping
+          </Button>
+        </div>
+        <div className='image-div'>
+          <Carousel images={images} />
+        </div>
       </div>
-      <div className='image-div'>
-        <Carousel images={images} />
+      <div className="multi-text">
+        <SentenceDisplay />
       </div>
     </div>
+
   );
 };
 
