35f762caa8bced694a4cfb205167ee256efcad6b
Gadoskey <gadoski321@gmail.com>
2025-01-03 20:26:16 +0100

    completed the categories. next dev each cart route

--- REACT_PRACTICE/src/assets/index.tsx
+++ REACT_PRACTICE/src/assets/index.tsx
@@ -1,9 +1,11 @@
-import top from './img/cart/topcart/topcartFuniture.png'
-import travel from './img/cart/topcart/topCartTravel.png'
-import snicker from './img/cart/topcart/topCartSnicker.png'
-import tech from './img/cart/topcart/topCartTech.png'
+import fashion from './img/cart/topcart/topcartFashion.png'
+import furniture from './img/cart/topcart/topcartFurniture.png'
+import gadgets from './img/cart/topcart/topCartGadgets.png'
+import beauty from './img/cart/topcart/topCartBeauty.png'
+import medicalSupplies from './img/cart/topcart/topCartMedicalSupplies.png'
+import sports from './img/cart/topcart/topCartSports.png'
+import toys from './img/cart/topcart/topCartToys.png'
 import books from './img/cart/topcart/topCartBook.png'
-import handbags from './img/cart/topcart/topCartHandbag.png'
 import basecamp from './img/cart/bestdeal/baseCamp.png'
 import homemini from './img/cart/bestdeal/homeMini.png'
 import mini9 from './img/cart/bestdeal/mini9.png'
@@ -17,4 +19,4 @@ import image7 from './img/cart/carousel/image7.jpeg';
 import image8 from './img/cart/carousel/image8.jpeg';
 import image9 from './img/cart/carousel/image9.jpeg';
 
-export { top, travel, snicker, tech, books, handbags , basecamp, homemini, mini9, image1, image2, image3, image4, image5, image6, image7, image8, image9 };
+export { fashion, furniture, gadgets, beauty, medicalSupplies, sports, books, toys, basecamp, homemini, mini9, image1, image2, image3, image4, image5, image6, image7, image8, image9 };
--- REACT_PRACTICE/src/components/cart/Topcartegories.tsx
+++ REACT_PRACTICE/src/components/cart/Topcartegories.tsx
@@ -1,5 +1,4 @@
-import { Col, Row } from 'react-bootstrap';
-import { books, handbags, snicker, tech, top, travel } from '../../assets';
+import { fashion, books, furniture, beauty, gadgets, medicalSupplies, toys, sports } from '../../assets';
 import '../../styles/components/cart/topcart.css';
 
 interface topcart {
@@ -10,80 +9,105 @@ interface topcart {
 function Topcartegories(): JSX.Element {
   const topCategories: topcart[] = [
     {
-      name: 'Funiture',
-      image: top,
+      name: 'Fashion',
+      image: fashion,
     },
     {
-      name: 'Hand Bags',
-      image: handbags,
+      name: 'Furniture',
+      image: furniture,
+    },
+    {
+      name: 'Gadgets',
+      image: gadgets,
+    },
+    {
+      name: 'Beauty',
+      image: beauty,
+    },
+    {
+      name: 'Medical',
+      image: medicalSupplies,
+    },
+    {
+      name: 'Sports',
+      image: sports,
     },
     {
       name: 'Books',
       image: books,
     },
     {
-      name: 'Tech',
-      image: tech,
-    },
-    {
-      name: 'Sneakers',
-      image: snicker,
-    },
-    {
-      name: 'Travel',
-      image: travel,
+      name: 'Toys',
+      image: toys,
     },
   ];
   return (
     <section>
-      <h2
-        style={{
-          lineHeight: '40px',
-          fontSize: '28px',
-          fontWeight: '700',
-          color: '#231f1e',
-          margin: '100px 0 10px 40px',
-        }}
-      >
-        Shop Our Top Categories
-      </h2>
-      <Row
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          gap: '9px',
-          justifyContent: 'space-between',
-          margin: '40px 20px 40px 0',
-          padding: '0 40px',
-        }}
-      >
-        {topCategories.map((cartgories: topcart) => (
-          <Col
-            className="topcart"
-            style={{
-              backgroundImage: `url(${cartgories.image})`,
-              height: '250px',
-              maxWidth: '200px',
-              minWidth: '200px',
-              backgroundSize: 'cover',
-              borderRadius: '10px',
-            }}
-          >
-            <h3
+      <div className='category-head'>
+        <div className='category-title'>
+          Shop Our Top Categories
+        </div>
+        <div className='category-grid topcart'>
+          {topCategories.map((category: topcart) => (
+            <div
+              key={category.name} // Ensure you have a unique key
               style={{
-                textAlign: 'center',
-                color: 'white',
-                fontSize: '35px',
-                fontWeight: '500',
-                marginTop: '25px',
+                position: 'relative',
+                height: '350px',
+                borderRadius: '8px',
+                overflow: 'hidden',
+                transition: 'transform 0.3s ease',
+              }}
+              onMouseEnter={(e) => {
+                e.currentTarget.style.transform = 'scale(1.05)';
+              }}
+              onMouseLeave={(e) => {
+                e.currentTarget.style.transform = 'scale(1)';
               }}
             >
-              {cartgories.name}
-            </h3>
-          </Col>
-        ))}
-      </Row>
+              <img
+                src={category.image}
+                alt={category.name}
+                style={{
+                  width: '100%',
+                  height: '100%',
+                  objectFit: 'cover',
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                }}
+              />
+              <h3
+                style={{
+                  position: 'absolute',
+                  top: '5px',
+                  left: '50%',
+                  transform: 'translateX(-50%)',
+                  color: 'white',
+                  fontSize: '25px',
+                  fontWeight: '800',
+                  margin: '0',
+                  textAlign: 'center',
+                }}
+              >
+                {category.name}
+              </h3>
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 }
 export default Topcartegories;
+
+/*
+Fashion
+Furniture
+Gadgets
+Beauty
+Medical Supplies
+Sports 
+Toys 
+Books
+*/
