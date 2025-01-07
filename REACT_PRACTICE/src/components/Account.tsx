7791af59c730cdd2be77b851c476cf8b3ff43a01
Gadoskey <gadoski321@gmail.com>
2025-01-07 18:43:46 +0100

    imlemented sign in ad sign up pages

--- REACT_PRACTICE/src/components/Account.tsx
+++ REACT_PRACTICE/src/components/Account.tsx
@@ -1,11 +1,16 @@
 import { Cart } from 'react-bootstrap-icons';
 import '../styles/components/Account.css';
+import { Link } from 'react-router-dom';
 
 function Account() {
   return (
     <div id="account" className='d-flex m-2 justify-content-between'>
-      <a href="" className="p-2">Sign In</a>
-      <a href="" className="p-2">Sign Up</a>
+      <Link to="/signin" className="p-2">
+        Sign In
+      </Link>
+      <Link to="/signup" className="p-2">
+        Sign Up
+      </Link>
       <span className="cart ms-5 p-2">
         <Cart className='cart-icon'/> Cart
       </span>
--- REACT_PRACTICE/src/main.tsx
+++ REACT_PRACTICE/src/main.tsx
@@ -1,20 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css';
+import App from './App.tsx';
+import SignIn from './pages/SignInPage';
+import SignUp from './pages/SignUpPage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 
 
-const router = createBrowserRouter([{
-  path: "/",
-  element: < App />,
-},
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/signin",
+    element: <SignIn />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
 ]);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router}>
-      
     </RouterProvider>
   </StrictMode>,
 )
